5fb29ceb2eb047ce1815e8a0a027a3d9d198ef35
Kan1aSorata <FengYuHe.BiliBili@hotmail.com>
2022-06-09 11:48:40 +0800

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by 贺峰煜 on 2022/6/9.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
